c8f25a57e247cb226dac6b776015c7422ac2bb2d
Sergey Scheglov <oxdeadbeef.ssch@gmail.com>
2022-03-24 22:02:51 +0700

    chore: Added in case of 15 errors proxy change, proxy regeneration every hour.

--- ts-balancer/main.ts
+++ ts-balancer/main.ts
@@ -4,37 +4,76 @@ import httpsProxyAgent from 'https-proxy-agent';
 
 const app = express();
 
-const proxies: [string, AxiosRequestConfig['httpsAgent']][] = [
-    "http://pjjLYy:6dL0xr@199.247.15.159:12588",
-	"http://pjjLYy:6dL0xr@199.247.15.159:12589",
-	"http://pjjLYy:6dL0xr@199.247.15.159:12587",
-	"http://pjjLYy:6dL0xr@199.247.15.159:12586",
-	"http://pjjLYy:6dL0xr@199.247.15.159:12585",
-	"http://d6pKGA:ZtxmsY@45.91.209.146:11875",
-	"http://d6pKGA:ZtxmsY@45.91.209.146:11874",
-	"http://d6pKGA:ZtxmsY@45.91.209.146:11873",
-	"http://d6pKGA:ZtxmsY@45.91.209.146:11872",
-	"http://d6pKGA:ZtxmsY@45.91.209.146:11871",
-	"http://pzQSM8:xevSX9@85.195.81.144:11329",
-	"http://pzQSM8:xevSX9@85.195.81.144:11328",
-	"http://pzQSM8:xevSX9@85.195.81.144:11327",
-	"http://pzQSM8:xevSX9@85.195.81.144:11326",
-	"http://pzQSM8:xevSX9@85.195.81.149:11685",
-	"http://RhbxQP:uEc4dA@104.238.190.248:10296",
-	"http://52qqy7:wX1MNS@85.195.81.143:10122",
-	"http://Uy8j3T:KJWZB2@45.145.57.228:11693",
-	"http://tUEGX8:bRXzV4@45.91.209.140:10484",
-	"http://dh3Ngq:q7BYyD@45.153.20.207:10487",
-].map((proxy) => [proxy, proxyParser(proxy)]);
+// const proxies: [string, AxiosRequestConfig['httpsAgent']][] = [
+//     "http://pjjLYy:6dL0xr@199.247.15.159:12588",
+// 	"http://pjjLYy:6dL0xr@199.247.15.159:12589",
+// 	"http://pjjLYy:6dL0xr@199.247.15.159:12587",
+// 	"http://pjjLYy:6dL0xr@199.247.15.159:12586",
+// 	"http://pjjLYy:6dL0xr@199.247.15.159:12585",
+// 	"http://d6pKGA:ZtxmsY@45.91.209.146:11875",
+// 	"http://d6pKGA:ZtxmsY@45.91.209.146:11874",
+// 	"http://d6pKGA:ZtxmsY@45.91.209.146:11873",
+// 	"http://d6pKGA:ZtxmsY@45.91.209.146:11872",
+// 	"http://d6pKGA:ZtxmsY@45.91.209.146:11871",
+// 	"http://pzQSM8:xevSX9@85.195.81.144:11329",
+// 	"http://pzQSM8:xevSX9@85.195.81.144:11328",
+// 	"http://pzQSM8:xevSX9@85.195.81.144:11327",
+// 	"http://pzQSM8:xevSX9@85.195.81.144:11326",
+// 	"http://pzQSM8:xevSX9@85.195.81.149:11685",
+// 	"http://RhbxQP:uEc4dA@104.238.190.248:10296",
+// 	"http://52qqy7:wX1MNS@85.195.81.143:10122",
+// 	"http://Uy8j3T:KJWZB2@45.145.57.228:11693",
+// 	"http://tUEGX8:bRXzV4@45.91.209.140:10484",
+// 	"http://dh3Ngq:q7BYyD@45.153.20.207:10487",
+// ].map((proxy) => [proxy, proxyParser(proxy)]);
+
+interface IProxy {
+    proxyUrl: string,
+    agent: AxiosRequestConfig['httpsAgent'],
+    errors: number
+}
+
+let proxies: IProxy[] = [];
+
+let currentPort = 0; // Current offset in proxy list
+let i = 0; // Current proxy index
+
+function generateProxy(): IProxy {
+    if(currentPort > 5000) {
+        currentPort = 0;
+    }
+    let proxyUrl = ''
+    if(currentPort < 2500) {
+        proxyUrl = 'http://vpsville:Pae9aile@45.139.185.34:';
+    } else {
+        proxyUrl = 'http://vpsville:Pae9aile@185.230.140.70:';
+    }
+    proxyUrl += currentPort++ % 2500 + 10001;
+    return {proxyUrl, agent: proxyParser(proxyUrl), errors: 0};
+}
+
+function intervalRegenProxy() {
+    i = 0;
+    let newProxy: IProxy[] = [];
+    for(let z = 1; z <= 100; z++) {
+        newProxy.push(generateProxy());
+    }
+    proxies = newProxy;
+}
+
+// First generate
+intervalRegenProxy()
+
+// Interval generate
+setInterval(intervalRegenProxy, 3600*1e3);
 
 
-let i = 0;
 
-function getRandomProxy(): [string, AxiosRequestConfig['httpsAgent']] {
-    if(i > proxies.length) {
+function getRandomProxy(): [IProxy, number] {
+    if(i >= proxies.length) {
         i = 0;
     }
-    return proxies[i++];
+    return [proxies[i++], i];
 }
 
 function proxyParser(proxy: string): AxiosRequestConfig['httpsAgent'] {
@@ -54,16 +93,26 @@ function userAgentGenerator(): string {
     return "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36";
 }
 
+function reportBug(proxyIndex: number, proxyUrl: string) {
+    const proxy = proxies[proxyIndex];
+    if(proxy.proxyUrl == proxyUrl) {
+        proxy.errors += 1;
+        if(proxy.errors >= 15) {
+            proxies[proxyIndex] = generateProxy();
+        }
+    }
+}
+
 export async function createRequest(url: string, req: express.Request<{}, any, any, Record<string, any>>): Promise<any> {
     let response;
     for(let i = 0; i < 5; i++) {
-        const [proxyURL, proxy] = getRandomProxy();
+        const [{proxyUrl, agent}, ProxyIndex] = getRandomProxy();
         // const proxy = proxyParser(proxyURL);
         try {
             if(req.method == 'GET') {
                 response = (await axios.get(url, {
                     // proxy,
-                    httpsAgent: proxy,
+                    httpsAgent: agent,
                     data: req.body,
                     headers: { 'User-Agent': userAgentGenerator() },
                     transformResponse: (r) => r
@@ -71,16 +120,17 @@ export async function createRequest(url: string, req: express.Request<{}, any, a
             } else {
                 response = (await axios.post(url, req.body, {
                     // proxy,
-                    httpsAgent: proxy,
+                    httpsAgent: agent,
                     headers: { 'User-Agent': userAgentGenerator() },
                     transformResponse: (r) => r
                 })).data
             }
-            console.log(req.method, url, proxyURL, req.body);
+            console.log(req.method, url, proxyUrl, req.body);
             break;
         } catch(err) {
-            console.log(req.method, url, proxyURL, req.body);
+            console.log(req.method, url, proxyUrl, req.body);
             console.log("Error: ", err);
+            reportBug(ProxyIndex, proxyUrl);
             continue;
         }
     }
